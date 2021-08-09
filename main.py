569af4c1a5170a7d0fd0015bc52103abb502bb39
Sebastian Hamza <sebby_99@yahoo.com>
2021-08-09 15:16:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import qrcode
+ 
+input_data = "https://github.com/sebastianhamza"
+
+#Instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=2)
+qr.add_data(input_data)
+qr.make(fit=True)
+
+img = qr.make_image(fill='black', back_color='white')
+
+img.save('qrcode001.png')
